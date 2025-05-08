0a67942e3709fc0c31ac551253d511c581455246
Guang Hu <ghu1@student.unimelb.edu.au>
2025-05-09 08:52:59 +1000

    added muddy children to quick test

--- utils/quick_validator.sh
+++ utils/quick_validator.sh
@@ -7,7 +7,7 @@ python -m jp.instance_runner -s jp/search_algorithms/bfsdc.py -e benchmarks/corr
 python -m jp.instance_runner -s jp/search_algorithms/bfsdc.py -e benchmarks/grapevine/grapevine.py -d benchmarks/grapevine/domain.pddl -p benchmarks/grapevine/problem01_4ag_2g_1d.pddl 
 python -m jp.instance_runner -s jp/search_algorithms/greedy.py -e benchmarks/grid/grid.py -d benchmarks/grid/domain.pddl -p benchmarks/grid/problem_EpGoal_prob1.pddl
 python -m jp.instance_runner -s jp/search_algorithms/greedy.py -e benchmarks/new_corridor/new_corridor.py -d benchmarks/new_corridor/domain.pddl -p benchmarks/new_corridor/problem01_3ag_2g_1d.pddl
-
+python -m jp.instance_runner -s jp/search_algorithms/bfsdc.py -e benchmarks/muddy_children/muddy_children.py -d benchmarks/muddy_children/domain.pddl -p benchmarks/muddy_children/problem02.pddl 
 
 
 
